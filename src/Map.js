f49aba3c49b33ed0aadc8510f2694d9c3d1c58c0
mtcolvard <matthew.colvard@gmail.com>
2020-12-06 06:19:43 +0100

    configuring data join for react hooks

--- src/Map.js
+++ src/Map.js
@@ -123,50 +123,6 @@ const Map = () => {
     })
   }
 
-  let data = election_data
-
-
-  map.on('load', function () {
-
-  // Add source for country polygons using the Mapbox Countries tileset
-  // The polygons contain an ISO 3166 alpha-3 code which can be used to for joining the data
-  // https://docs.mapbox.com/vector-tiles/reference/mapbox-countries-v1
-  map.addSource('countries', {
-  type: 'vector',
-  url: 'mapbox://mapbox.country-boundaries-v1'
-  })
-
-  // Build a GL match expression that defines the color for every vector tile feature
-  // Use the ISO 3166-1 alpha 3 code as the lookup key for the country shape
-  var matchExpression = ['match', ['get', 'iso_3166_1_alpha_3']]
-
-  // Calculate color values for each country based on 'hdi' value
-  data.forEach(function (row) {
-
-  // Convert the range of data values to a suitable color
-  var green = row['hdi'] * 255
-  var color = 'rgb(0, '+ green + ', 0)'
-
-  matchExpression.push(row['code'], color)
-
-  })
-
-  // Last value is the default, used where there is no data
-  matchExpression.push('rgba(0, 0, 0, 0)')
-
-  // Add layer from the vector tile source to create the choropleth
-  // Insert it below the 'admin-1-boundary-bg' layer in the style
-  map.addLayer({
-  'id': 'countries-join',
-  'type': 'fill',
-  'source': 'countries',
-  'source-layer': 'country_boundaries',
-  'paint': {
-  'fill-color': matchExpression
-  }
-  }, 'admin-1-boundary-bg')
-
-  })
 
   return (
     <div>
