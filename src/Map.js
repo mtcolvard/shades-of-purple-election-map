d18ad2e72e70abc797a36b8a9878cdfe75b88dc8
mtcolvard <matthew.colvard@gmail.com>
2020-12-08 03:33:56 +0100

    commented out legend and options. about to render and test

--- src/Map.js
+++ src/Map.js
@@ -10,45 +10,45 @@ mapboxgl.accessToken =
   'pk.eyJ1IjoibXRjb2x2YXJkIiwiYSI6ImNraHF2MXA4aDBkajUyem1zaXRmYWJjbDUifQ.97qiz4KJ02kEjzajDF-WFw'
 
 
-const Map = () => {
-  const options = [
-    {
-      name: 'Population',
-      description: 'Estimated total population',
-      property: 'pop_est',
-      stops: [
-        [0, '#f8d5cc'],
-        [1000000, '#f4bfb6'],
-        [5000000, '#f1a8a5'],
-        [10000000, '#ee8f9a'],
-        [50000000, '#ec739b'],
-        [100000000, '#dd5ca8'],
-        [250000000, '#c44cc0'],
-        [500000000, '#9f43d7'],
-        [1000000000, '#6e40e6']
-      ]
-    },
-    {
-      name: 'GDP',
-      description: 'Estimate total GDP in millions of dollars',
-      property: 'gdp_md_est',
-      stops: [
-        [0, '#f8d5cc'],
-        [1000, '#f4bfb6'],
-        [5000, '#f1a8a5'],
-        [10000, '#ee8f9a'],
-        [50000, '#ec739b'],
-        [100000, '#dd5ca8'],
-        [250000, '#c44cc0'],
-        [5000000, '#9f43d7'],
-        [10000000, '#6e40e6']
-      ]
-    }
-  ]
+// const Map = () => {
+  // const options = [
+  //   {
+  //     name: 'Population',
+  //     description: 'Estimated total population',
+  //     property: 'pop_est',
+  //     stops: [
+  //       [0, '#f8d5cc'],
+  //       [1000000, '#f4bfb6'],
+  //       [5000000, '#f1a8a5'],
+  //       [10000000, '#ee8f9a'],
+  //       [50000000, '#ec739b'],
+  //       [100000000, '#dd5ca8'],
+  //       [250000000, '#c44cc0'],
+  //       [500000000, '#9f43d7'],
+  //       [1000000000, '#6e40e6']
+  //     ]
+  //   },
+  //   {
+  //     name: 'GDP',
+  //     description: 'Estimate total GDP in millions of dollars',
+  //     property: 'gdp_md_est',
+  //     stops: [
+  //       [0, '#f8d5cc'],
+  //       [1000, '#f4bfb6'],
+  //       [5000, '#f1a8a5'],
+  //       [10000, '#ee8f9a'],
+  //       [50000, '#ec739b'],
+  //       [100000, '#dd5ca8'],
+  //       [250000, '#c44cc0'],
+  //       [5000000, '#9f43d7'],
+  //       [10000000, '#6e40e6']
+  //     ]
+  //   }
+  // ]
   const mapContainerRef = useRef(null)
-  const [active, setActive] = useState(options[0])
+  // const [active, setActive] = useState(options[0])
   const [map, setMap] = useState(null)
-  const electionData = election_data
+  // const electionData = election_data
 
   // Initialize map when component mounts
   useEffect(() => {
@@ -81,10 +81,10 @@ const Map = () => {
           }
         })
 
-      map.setPaintProperty('stateBoundaries', 'fill-color', {
-        property: active.property,
-        stops: active.stops
-      })
+      // map.setPaintProperty('stateBoundaries', 'fill-color', {
+      //   property: active.property,
+      //   stops: active.stops
+      // })
 
       setMap(map)
     })
@@ -93,46 +93,46 @@ const Map = () => {
     return () => map.remove()
   }, [])
 
-  useEffect(() => {
-    paint()
-  }, [active])
-
-  const paint = () => {
-    if (map) {
-      map.setPaintProperty('data', 'fill-color', {
-        property: active.property,
-        stops: active.stops
-      })
-    }
-  }
-
-  const changeState = i => {
-    setActive(options[i])
-    map.setPaintProperty('data', 'fill-color', {
-      property: active.property,
-      stops: active.stops
-    })
-  }
-
-  let matchExpression = ['match', ['get', 'STATEFP']]
-
-  electionData.forEach( electionResult => {
-    const color = stateElection['dem_percent_2020']
-
-  })
+  // useEffect(() => {
+  //   paint()
+  // }, [active])
+  //
+  // const paint = () => {
+  //   if (map) {
+  //     map.setPaintProperty('data', 'fill-color', {
+  //       property: active.property,
+  //       stops: active.stops
+  //     })
+  //   }
+  // }
+  //
+  // const changeState = i => {
+  //   setActive(options[i])
+  //   map.setPaintProperty('data', 'fill-color', {
+  //     property: active.property,
+  //     stops: active.stops
+  //   })
+  // }
+  //
+  // let matchExpression = ['match', ['get', 'STATEFP']]
+  //
+  // electionData.forEach( electionResult => {
+  //   const color = stateElection['dem_percent_2020']
+  // })
 
 
   return (
     <div>
       <div ref={mapContainerRef} className='map-container' />
-      <Legend active={active} stops={active.stops} />
-      <Optionsfield
-        options={options}
-        property={active.property}
-        changeState={changeState}
-      />
     </div>
   )
 }
 
 export default Map
+
+<Legend active={active} stops={active.stops} />
+<Optionsfield
+  options={options}
+  property={active.property}
+  changeState={changeState}
+/>
