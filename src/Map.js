45b69c05e741b6d52a80b814768b4c9597c7d11a
mtcolvard <matthew.colvard@gmail.com>
2020-12-09 05:29:14 +0100

    check the compile terminal log. your hooks have two missing dependencies

--- src/Map.js
+++ src/Map.js
@@ -1,55 +1,31 @@
-// import React, { useRef, useEffect, useState } from 'react'
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
+// import React, { useRef, useEffect } from 'react'
 import mapboxgl from 'mapbox-gl'
 import './Map.css'
 // import Legend from './components/Legend'
-// import Optionsfield from './components/Optionsfield'
+import Optionsfield from './components/Optionsfield'
 import electionNumbers from'./election_data_join.geojson'
-// import data from './state_geometry.json'
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoibXRjb2x2YXJkIiwiYSI6ImNraHF2MXA4aDBkajUyem1zaXRmYWJjbDUifQ.97qiz4KJ02kEjzajDF-WFw'
 
 
 const Map = () => {
-  // const options = [
-  //   {
-  //     name: 'Population',
-  //     description: 'Estimated total population',
-  //     property: 'pop_est',
-  //     stops: [
-  //       [0, '#f8d5cc'],
-  //       [1000000, '#f4bfb6'],
-  //       [5000000, '#f1a8a5'],
-  //       [10000000, '#ee8f9a'],
-  //       [50000000, '#ec739b'],
-  //       [100000000, '#dd5ca8'],
-  //       [250000000, '#c44cc0'],
-  //       [500000000, '#9f43d7'],
-  //       [1000000000, '#6e40e6']
-  //     ]
-  //   },
-  //   {
-  //     name: 'GDP',
-  //     description: 'Estimate total GDP in millions of dollars',
-  //     property: 'gdp_md_est',
-  //     stops: [
-  //       [0, '#f8d5cc'],
-  //       [1000, '#f4bfb6'],
-  //       [5000, '#f1a8a5'],
-  //       [10000, '#ee8f9a'],
-  //       [50000, '#ec739b'],
-  //       [100000, '#dd5ca8'],
-  //       [250000, '#c44cc0'],
-  //       [5000000, '#9f43d7'],
-  //       [10000000, '#6e40e6']
-  //     ]
-  //   }
-  // ]
+  const options = [
+    {
+      name: '2020',
+      description: '2020 Election Results',
+      property: 'Purple Maps State Results edit_dem_total',
+    },
+    {
+      name: '2016',
+      description: '2016 Election Results',
+      property: 'Purple Maps State Results edit_rep_total',
+    }
+  ]
   const mapContainerRef = useRef(null)
-  // const [active, setActive] = useState(options[0])
-  // const [map, setMap] = useState(null)
-  // const electionData = election_data
+  const [active, setActive] = useState(options[0])
+  const [map, setMap] = useState(null)
 
   // Initialize map when component mounts
   useEffect(() => {
@@ -76,7 +52,23 @@ const Map = () => {
         'type': 'geojson',
         'data': electionNumbers
       })
-
+// this addLayer version works
+      // map.addLayer(
+      //   {
+      //     'id': 'purpleShade',
+      //     'type': 'fill',
+      //     'source': 'electionNumbers',
+      //     // 'source-layer': 'purple-maps-data-join-2-6th2yr',
+      //     'paint': {
+      //       'fill-color': [
+      //         'interpolate',
+      //         ['linear'],
+      //         ['*',['to-number',['get','Purple Maps State Results edit_dem_total']],100],
+      //         0, '#ff0000', 100, '#0000ff'
+      //       ]
+      //     }
+      //   })
+// this addLayer version is experimental
       map.addLayer(
         {
           'id': 'purpleShade',
@@ -84,68 +76,61 @@ const Map = () => {
           'source': 'electionNumbers',
           // 'source-layer': 'purple-maps-data-join-2-6th2yr',
           'paint': {
-            'fill-color': [
-              'interpolate',
-              ['linear'],
-              ['*',['to-number',['get','Purple Maps State Results edit_dem_total']],100],
-              0, '#ff0000', 100, '#0000ff'
-            ]
+            'fill-color':
+            ['interpolate',['linear'],['*',['to-number',['get', 'property']],100],
+              0, '#ff0000', 100, '#0000ff']
           }
         })
 
-      // map.setPaintProperty('stateBoundaries', 'fill-color', {
-      //   property: active.property,
-      //   stops: active.stops
-      // })
+      map.setPaintProperty('purpleShade', 'fill-color', ['interpolate',['linear'],['*',['to-number',['get', 'property']],100],
+        0, '#ff0000', 100, '#0000ff'], {
+        property: active.property
+      })
 
-      // setMap(map)
+      setMap(map)
     })
 
     // Clean up on unmount
     return () => map.remove()
   }, [])
 
-  // useEffect(() => {
-  //   paint()
-  // }, [active])
-  //
-  // const paint = () => {
-  //   if (map) {
-  //     map.setPaintProperty('data', 'fill-color', {
-  //       property: active.property,
-  //       stops: active.stops
-  //     })
-  //   }
-  // }
-  //
-  // const changeState = i => {
-  //   setActive(options[i])
-  //   map.setPaintProperty('data', 'fill-color', {
-  //     property: active.property,
-  //     stops: active.stops
-  //   })
-  // }
-  //
-  // let matchExpression = ['match', ['get', 'STATEFP']]
-  //
-  // electionData.forEach( electionResult => {
-  //   const color = stateElection['dem_percent_2020']
-  // })
+  useEffect(() => {
+    paint()
+  }, [active])
 
-  // <span className='leftGradient'>There are no </span><span className='red'>red</span> <span className='rightGradient'>states or</span> <span className='blue'>blue</span> states.
-
-
+  const paint = () => {
+    if (map) {
+      map.setPaintProperty('purpleShade', 'fill-color', ['interpolate',['linear'],['*',['to-number',['get', 'property']],100],
+        0, '#ff0000', 100, '#0000ff'], {
+        property: active.property
+      })
+    }
+  }
+  //
+  const changeState = i => {
+    setActive(options[i])
+    map.setPaintProperty('purpleShade', 'fill-color', ['interpolate',['linear'],['*',['to-number',['get', 'property']],100],
+      0, '#ff0000', 100, '#0000ff'], {
+      property: active.property
+    })
+  }
 
   return (
     <div>
-      <h1 className='headline'>
-      <div className='lineOne'> There are no red states or blue states.<br /><br />
-      <span className='lineTwo'>Only the United States.</span><br /><br />
-      <span className='lineThree'>Shades of Purple</span>
+      <div>
+        <h1 className='headline'>
+          <div className='lineOne'> There are no red states or blue states.<br /><br />
+          <span className='lineTwo'>Only the United States.</span><br /><br />
+          <span className='lineThree'>Shades of Purple</span>
+          </div>
+        </h1>
       </div>
-      </h1>
-
       <div ref={mapContainerRef} className='map-container' />
+      <Optionsfield
+        options={options}
+        property={active.property}
+        changeState={changeState}
+      />
     </div>
   )
 }
@@ -153,8 +138,4 @@ const Map = () => {
 export default Map
 
 // <Legend active={active} stops={active.stops} />
-// <Optionsfield
-//   options={options}
-//   property={active.property}
-//   changeState={changeState}
-// />
+//
