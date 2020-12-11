a9065462454bb3c7a25a95d9e651fecc710bd296
mtcolvard <matthew.colvard@gmail.com>
2020-12-11 19:41:39 +0100

    working on turning the array of 2020 election results into objects

--- utilities/json_converter.py
+++ utilities/json_converter.py
@@ -1,18 +1,20 @@
 import json
 # import .election_data_join.geojson
 
-with open('election_data_join.geojson') as f:
-    data = json.load(f)
-    for item in data['features']:
-        item['id'] = int(item['properties']['GEOID'])
-
-    with open('election_data_join.geojson', 'w') as f:
-        json.dump(data, f)
-
-# with open('2020_election_results.json') as f:
+# with open('election_data_join.geojson') as f:
 #     data = json.load(f)
-#     for item in data:
-#         item['FIPS'] = int(item['FIPS'])
+#     for item in data['features']:
+#         item['id'] = int(item['properties']['GEOID'])
 #
-#     with open('2020_election_results.json', 'w') as f:
+#     with open('election_data_join.geojson', 'w') as f:
 #         json.dump(data, f)
+
+with open('election_results_2020.json') as f:
+    data = json.load(f)
+    for item in data:
+        item['id'] = int(item['FIPS'])
+        # item['2020_dem_pct'] = int(item['2020_dem_pct'])
+
+
+    with open('election_results_2020.json', 'w') as f:
+        json.dump(data, f)
