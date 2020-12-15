333c4b7c39019b4b0deab136554e6a85a5330475
mtcolvard <matthew.colvard@gmail.com>
2020-12-15 17:22:51 +0100

    compiled the current and  all the historical into on geojson file, rather than try to make multiple layers work, and uploaded to studio. the paint issue turned out to be incorrect syntax on setPaintProperty. the expression follows fill-color after a comma.  not seperately as an object.  everything working now back through 1988.

--- src/Maps.js
+++ src/Maps.js
@@ -15,86 +15,80 @@ const Maps = () => {
     {
       name: '2020',
       description: '2020 Election Results',
-      property: '2020_dem_pct',
-      stops: [[0, '#FF0000'],[100, '#0000FF']]
+      property: '2020_dem_perc',
     },
     {
       name: '2016',
       description: '2016 Election Results',
-      property: '2016_dem_pct',
-      stops: [[0, '#FF0000'],[100, '#0000FF']]
+      property: '2016_dem_perc',
     },
     {
       name: '2012',
       description: '2012 Election Results',
-      property: '2012_dem_pct',
-      stops: [[0, '#FF0000'],[100, '#0000FF']]
-
+      property: '2012_dem_perc',
+    },
+    {
+      name: '2008',
+      description: '2008 Election Results',
+      property: '2008_dem_perc',
+    },
+    {
+      name: '2004',
+      description: '2004 Election Results',
+      property: '2004_dem_perc',
+    },
+    {
+      name: '2000',
+      description: '2000 Election Results',
+      property: '2000_dem_perc',
+    },
+    {
+      name: '1996',
+      description: '1996 Election Results',
+      property: '1996_dem_perc',
+    },
+    {
+      name: '1992',
+      description: '1992 Election Results',
+      property: '1992_dem_perc',
+    },
+    {
+      name: '1988',
+      description: '1988 Election Results',
+      property: '1988_dem_perc',
     }
   ]
 
   const mapContainerRef = useRef(null)
   const [active, setActive] = useState(options[0])
   const [map, setMap] = useState(null)
+  const fillColorExpression = ['interpolate', ['linear'], ['*', ['to-number', ['get', active.property]], 100], 0, '#ff0000', 100, '#0000ff']
 
   // Initialize map when component mounts
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: 'mapbox://styles/mtcolvard/ckifazn3008wo19o2faxvjc7c',
-      // style: 'mapbox://styles/mtcolvard/ckip70h8a3vgu17mu2x5q8wj5',
+      style: 'mapbox://styles/mtcolvard/ckiq46ygw162e17nkl6jgkccj',
       center: [-95.4, 37.6],
       zoom: 2.5
     })
     // let newdata = new Map()
 
     map.on('load', () => {
-
       map.addSource('vectorElectionNumbers', {
         'type': 'vector',
-        'data': 'mapbox://styles/mtcolvard/ckifazn3008wo19o2faxvjc7c',
-        // 'generateId': true
-        // 'data': 'mapbox://styles/mtcolvard/ckip70h8a3vgu17mu2x5q8wj5',
+        'data': 'mapbox://styles/mtcolvard/ckiq46ygw162e17nkl6jgkccj',
       })
 
-      map.addLayer(
-        {
-          'id': 'historical-pres-elections-state',
+      map.addLayer({
+          'id': 'purple-maps-master-edit-geojs-9gxg35',
           'type': 'fill',
           'source': 'vectorElectionNumbers',
-          'source-layer': 'historical-pres-elections-state',
-      //     // 'paint': {
-      //       // 'fill-color': [
-      //       //   'interpolate',
-      //       //   ['linear'],
-      //       //   ['to-number',['get', active.property]],
-      //       //   0, '#ff0000', 100, '#0000ff'
-      //       // ]
-      //     // }
-    })
-      map.setPaintProperty('historical-pres-elections-state', 'fill-color', {
-        property: active.property,
-        stops: active.stops
+          'source-layer': 'purple-maps-master-edit-geojs-9gxg35',
       })
-      map.addLayer(
-        {
-          'id': 'fips-fixed-export-0s3h8j',
-          'type': 'fill',
-          'source': 'vectorElectionNumbers',
-          'source-layer': 'fips-fixed-export-0s3h8j',
-          // 'paint': {
-          //   'fill-color': [
-          //     'interpolate',
-          //     ['linear'],
-          //     ['*',['to-number',['get', active.property]],100],
-          //     0, '#ff0000', 100, '#0000ff'
-          //   ]
-          // }
-        })
-      // map.setPaintProperty('fips-fixed-export-0s3h8j', 'fill-color', {
-      //   property: active.property,
-      //   stops: active.stops
-      // })
+
+      map.setPaintProperty('purple-maps-master-edit-geojs-9gxg35', 'fill-color', fillColorExpression
+      )
 
       setMap(map)
     })
@@ -109,45 +103,35 @@ const Maps = () => {
 
   const paint = () => {
     if (map) {
-      // map.setPaintProperty('fips-fixed-export-0s3h8j', 'fill-color', {
-      //   property: active.property,
-      //   stops: active.stops
-      // })
-      map.setPaintProperty('historical-pres-elections-state', 'fill-color', {
-        property: active.property,
-        stops: active.stops
-      })
+      map.setPaintProperty('purple-maps-master-edit-geojs-9gxg35', 'fill-color', fillColorExpression)
     }
   }
   //
   const changeState = i => {
     setActive(options[i])
-    // map.setPaintProperty('fips-fixed-export-0s3h8j', 'fill-color', {
-    //   property: active.property,
-    //   stops: active.stops
-    // })
-    map.setPaintProperty('historical-pres-elections-state', 'fill-color', {
-      property: active.property,
-      stops: active.stops
-    })
+    map.setPaintProperty('purple-maps-master-edit-geojs-9gxg35', 'fill-color', fillColorExpression)
   }
 // This is a map that show the votes of the American people.  The shades of the map are a mix of primary red and primary blue depending on the proportion of votes cast in each state. ..how each state voted
   return (
     <div>
-      <div>
+      <div className="wrapper">
+        <header className='header'>
         <h1 className='headline'>
           <div className='lineOne'> There are no red states or blue states.<br /><br />
           <span className='lineTwo'>Only the United States.</span><br /><br />
           <span className='lineThree'>Shades of Purple</span>
           </div>
         </h1>
+        </header>
+        <article className='main'>
+        <Optionsfield
+          options={options}
+          property={active.property}
+          changeState={changeState}
+        />
+        </article>
       </div>
       <div ref={mapContainerRef} className='map-container' />
-      <Optionsfield
-        options={options}
-        property={active.property}
-        changeState={changeState}
-      />
     </div>
   )
 }
