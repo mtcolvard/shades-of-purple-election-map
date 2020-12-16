c0b2946ae4686ce5f7cb83e5c684f9e4fd7eaed6
mtcolvard <matthew.colvard@gmail.com>
2020-12-16 04:14:20 +0100

    added calculations for legend display

--- src/Maps.js
+++ src/Maps.js
@@ -2,66 +2,148 @@ import React, { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 import './Map.css'
 import Optionsfield from './components/Optionsfield'
-// import Legend from './components/Legend'
+import Legend from './components/Legend'
+import electionData from './historicElectionResults.json'
+var _ = require('lodash')
+
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoibXRjb2x2YXJkIiwiYSI6ImNraHF2MXA4aDBkajUyem1zaXRmYWJjbDUifQ.97qiz4KJ02kEjzajDF-WFw'
 
 const Maps = () => {
 
-  const mapContainerRef = useRef(null)
-  const [active, setActive] = useState(options[0])
-  const [map, setMap] = useState(null)
-
-  const fillColorExpression = ['interpolate', ['linear'], ['*', ['to-number', ['get', active.property]], 100], 0, '#ff0000', 100, '#0000ff']
-
   const options = [
     {
       name: '2020',
       description: '2020 Election Results',
       property: '2020_dem_perc',
+      dem_data: 'dem_total_2020',
+      rep_data: 'rep_total_2020'
     },
     {
       name: '2016',
       description: '2016 Election Results',
       property: '2016_dem_perc',
+      dem_data: 'dem_total_2016',
+      rep_data: 'rep_total_2016'
+
     },
     {
       name: '2012',
       description: '2012 Election Results',
       property: '2012_dem_perc',
+      dem_data: 'dem_total_2012',
+      rep_data: 'rep_total_2012'
+
     },
     {
       name: '2008',
       description: '2008 Election Results',
       property: '2008_dem_perc',
+      dem_data: 'dem_total_2008',
+      rep_data: 'rep_total_2008'
+
     },
     {
       name: '2004',
       description: '2004 Election Results',
       property: '2004_dem_perc',
+      dem_data: 'dem_total_2004',
+      rep_data: 'rep_total_2004'
+
     },
     {
       name: '2000',
       description: '2000 Election Results',
       property: '2000_dem_perc',
+      dem_data: 'dem_total_2000',
+      rep_data: 'rep_total_2000'
+
     },
     {
       name: '1996',
       description: '1996 Election Results',
       property: '1996_dem_perc',
+      dem_data: 'dem_total_1996',
+      rep_data: 'rep_total_1996'
+
     },
     {
       name: '1992',
       description: '1992 Election Results',
       property: '1992_dem_perc',
+      dem_data: 'dem_total_1992',
+      rep_data: 'rep_total_1992'
+
     },
     {
       name: '1988',
       description: '1988 Election Results',
       property: '1988_dem_perc',
+      dem_data: 'dem_total_1988',
+      rep_data: 'rep_total_1988'
     }
   ]
+
+  const stateKey = {
+  '01': 'Alabama',
+  '02': 'Alaska',
+  '04': 'Arizona',
+  '05': 'Arkansas',
+  '06': 'California',
+  '08': 'Colorado',
+  '09': 'Connecticut',
+  '10': 'Delaware',
+  '11': 'District of Columbia',
+  '12': 'Florida',
+  '13': 'Georgia',
+  '15': 'Hawaii',
+  '16': 'Idaho',
+  '17': 'Illinois',
+  '18': 'Indiana',
+  '19': 'Iowa',
+  '20': 'Kansas',
+  '21': 'Kentucky',
+  '22': 'Louisiana',
+  '23': 'Maine',
+  '24': 'Maryland',
+  '25': 'Massachusetts',
+  '26': 'Michigan',
+  '27': 'Minnesota',
+  '28': 'Mississippi',
+  '29': 'Missouri',
+  '30': 'Montana',
+  '31': 'Nebraska',
+  '32': 'Nevada',
+  '33': 'New Hampshire',
+  '34': 'New Jersey',
+  '35': 'New Mexico',
+  '36': 'New York',
+  '37': 'North Carolina',
+  '38': 'North Dakota',
+  '39': 'Ohio',
+  '40': 'Oklahoma',
+  '41': 'Oregon',
+  '42': 'Pennsylvania',
+  '44': 'Rhode Island',
+  '45': 'South Carolina',
+  '46': 'South Dakota',
+  '47': 'Tennessee',
+  '48': 'Texas',
+  '49': 'Utah',
+  '50': 'Vermont',
+  '51': 'Virginia',
+  '53': 'Washington',
+  '54': 'West Virginia',
+  '55': 'Wisconsin',
+  '56': 'Wyoming'}
+
+
+  const mapContainerRef = useRef(null)
+  const [active, setActive] = useState(options[0])
+  const [map, setMap] = useState(null)
+
+  const fillColorExpression = ['interpolate', ['linear'], ['*', ['to-number', ['get', active.property]], 100], 0, '#ff0000', 100, '#0000ff']
 
 
 
@@ -90,7 +172,6 @@ const Maps = () => {
 
       map.setPaintProperty('purple-maps-master-edit-geojs-9gxg35', 'fill-color', fillColorExpression
       )
-
       setMap(map)
     })
 
@@ -101,6 +182,33 @@ const Maps = () => {
   useEffect(() => {
     paint()
   }, [active])
+
+ useEffect(() => {
+   testData()
+ })
+
+  const testData = () => {
+
+    // const demElectionYearData = electionData[active.dem_data]
+    const demElectionYearData = _.omit(electionData[active.dem_data], ['11'])
+
+    const repElectionYearData = electionData[active.rep_data]
+    _.omit(repElectionYearData, ['11'])
+
+
+    const mostPartisanDemFIPS = _.maxBy(_.keys(demElectionYearData), o => demElectionYearData[o])
+    const mostPartisanDemState = stateKey[mostPartisanDemFIPS]
+    const mostPartisanDemStatePercent = demElectionYearData[mostPartisanDemFIPS]
+
+    const mostPartisanRepFIPS = _.maxBy(_.keys(repElectionYearData), o => repElectionYearData[o])
+    const mostPartisanRepState = stateKey[mostPartisanRepFIPS]
+    const mostPartisanRepStatePercent = repElectionYearData[mostPartisanRepFIPS]
+
+    console.log('mostPartisanDemState', mostPartisanDemState, mostPartisanDemStatePercent)
+    console.log('mostPartisanRepState', mostPartisanRepState, mostPartisanRepStatePercent )
+
+  }
+
 
   const paint = () => {
     if (map) {
@@ -125,6 +233,7 @@ const Maps = () => {
         </h1>
         </header>
         <article className='main'>
+        <Legend active={active} stops={active.election} />
         <Optionsfield
           options={options}
           property={active.property}
@@ -138,52 +247,3 @@ const Maps = () => {
 }
 
 export default Maps
-
-// election_result_2020.forEach((election_result) => {
-//   const dem_result = election_result['2020_dem_pct']
-//   const id = election_result['FIPS']
-//   newdata[id] = { '2020_dem_pct': dem_result
-//   }
-// })
-//
-// const initLayers = () => {
-//   map.addSource('vectorElectionNumbers', {
-//     'type': 'vector',
-//     'data': 'mapbox://styles/mtcolvard/ckifazn3008wo19o2faxvjc7c',
-//   })
-//
-//   map.addLayer({
-//     'id': 'results_2020',
-//     'type': 'fill',
-//     'source': 'vectorElectionNumbers',
-//     'source-layer': 'results_new',
-//     'paint': {
-//       'fill-color': ['feature-state', 'color']
-//     }
-//   })
-//
-//   const setStatesColor = () => {
-//     for (let key in newdata) {
-//       map.setFeatureState({
-//         'source': 'vectorElectionNumbers',
-//         'sourceLayer': 'result_new',
-//         'id': key
-//       }, {
-//         'color': newdata[key]['2020_dem_pct']
-//       })
-//     }
-//   }
-//   const setAfterLoad = (e) => {
-//     if(e.sourceId === 'vectorElectionNumbers' && e.isSourceLoaded) {
-//       setStatesColor()
-//       map.off('sourcedata', setAfterLoad)
-//     }
-//   }
-//
-//   if (map.isSourceLoaded('vectorElectionNumbers')) {
-//     setStatesColor()
-//   } else {
-//     map.on('sourcedata', setAfterLoad)
-//   }
-// }
-// initLayers()
--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,17 +1,85 @@
-import React from "react";
+
+import React, { useEffect} from 'react';
+var _ = require('lodash')
 
 const Legend = (props) => {
-  const renderLegendKeys = (stop, i) => {
-    return (
-      <div key={i} className="txt-s">
-        <span
-          className="mr6 round-full w12 h12 inline-block align-middle"
-          style={{ backgroundColor: stop[1] }}
-        />
-        <span>{`${stop[0].toLocaleString()}`}</span>
-      </div>
-    );
-  };
+  const stateKey = {
+  '01': 'Alabama',
+  '02': 'Alaska',
+  '04': 'Arizona',
+  '05': 'Arkansas',
+  '06': 'California',
+  '08': 'Colorado',
+  '09': 'Connecticut',
+  '10': 'Delaware',
+  '11': 'District of Columbia',
+  '12': 'Florida',
+  '13': 'Georgia',
+  '15': 'Hawaii',
+  '16': 'Idaho',
+  '17': 'Illinois',
+  '18': 'Indiana',
+  '19': 'Iowa',
+  '20': 'Kansas',
+  '21': 'Kentucky',
+  '22': 'Louisiana',
+  '23': 'Maine',
+  '24': 'Maryland',
+  '25': 'Massachusetts',
+  '26': 'Michigan',
+  '27': 'Minnesota',
+  '28': 'Mississippi',
+  '29': 'Missouri',
+  '30': 'Montana',
+  '31': 'Nebraska',
+  '32': 'Nevada',
+  '33': 'New Hampshire',
+  '34': 'New Jersey',
+  '35': 'New Mexico',
+  '36': 'New York',
+  '37': 'North Carolina',
+  '38': 'North Dakota',
+  '39': 'Ohio',
+  '40': 'Oklahoma',
+  '41': 'Oregon',
+  '42': 'Pennsylvania',
+  '44': 'Rhode Island',
+  '45': 'South Carolina',
+  '46': 'South Dakota',
+  '47': 'Tennessee',
+  '48': 'Texas',
+  '49': 'Utah',
+  '50': 'Vermont',
+  '51': 'Virginia',
+  '53': 'Washington',
+  '54': 'West Virginia',
+  '55': 'Wisconsin',
+  '56': 'Wyoming'}
+
+
+  // useEffect(() => {
+  //   maxKey()
+  //   testData()
+  // }, [])
+
+  // let maxKey = () => { _.max(Object.keys(props.active.election), o => props.active.election[o])}
+  //
+  // const testData = () => {
+  //   console.log('maxKey', stateKey[0][maxKey])
+  //
+  //  }
+
+  // const renderLegendKeys = (election, i) => {
+  //   return (
+  //     <div key={i} className="txt-s">
+  //       <span
+  //         className="mr6 round-full w12 h12 inline-block align-middle"
+  //         style={{ backgroundColor: election[1] }}
+  //       />
+  //       <span>{`${stateKey[maxKey].toLocaleString()}`}</span>
+  //     </div>
+  //   );
+  // };
 
   return (
     <>
@@ -20,10 +88,18 @@ const Legend = (props) => {
           <h2 className="txt-bold txt-s block">{props.active.name}</h2>
           <p className="txt-s color-gray">{props.active.description}</p>
         </div>
-        {props.stops.map(renderLegendKeys)}
+
       </div>
     </>
   );
 };
 
 export default Legend;
+
+// <div  className="txt-s">
+//   <span
+//     className="mr6 round-full w12 h12 inline-block align-middle"
+//     style={{ backgroundColor: '#FF0000' }}
+//   />
+//   <span>{`${stateKey[maxKey]}`}</span>
+// </div>
